f7e7f2399a993db764a44904e0c6dbdbce53a520
jennifer.dai@berkeley.edu <jennifer.dai@berkeley.edu>
2015-02-22 12:19:20 -0800

    added toggling temp

--- app/testing_broadcast.lua
+++ app/testing_broadcast.lua
@@ -32,10 +32,28 @@ a_socket = storm.net.udpsocket(1525,
 							   msg = print_msg(msg)
 							end)
 local msg = storm.mp.pack(svc_manifest)
-	storm.os.invokePeriodically(5*storm.os.SECOND, function()
-        		storm.net.sendto(a_socket, msg, "ff02::1", 1525)
-			print("PLEASE")
-		end)
+storm.os.invokePeriodically(5*storm.os.SECOND, function()
+       		storm.net.sendto(a_socket, msg, "ff02::1", 1525)
+	end)
+
+local svc_invoke_off = {
+     "setOvenState",
+     {false},
+}
+local svc_invoke_on = {
+     "setOvenState",
+     {true},
+}
+local onMsg = storm.mp.pack(svc_invoke_on)
+local offMsg = storm.mp.pack(svc_invoke_off)
+function toggleState(args)
+	print("i want to turn the oven on")
+	if args then
+	     storm.net.sendto(a_socket, onMsg, "ff02::1", 1526) 
+        else
+	     storm.net.sendto(a_socket, offMsg, "ff02::1", 1526) 
+        end
+end
 sh = require "stormsh"
 sh.start()
 cord.enter_loop()
